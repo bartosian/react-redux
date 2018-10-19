81c680a905496915523977c09675c867ca8a8c34
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-19 15:04:23 -0400

    Added like-dislike feature

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 class Stream extends React.Component {
 
+    constructor() {
+        super();
+        this.state = {};
+    }
+
     render() {
         const { tracks = [] } = this.props;
 
@@ -9,12 +14,20 @@ class Stream extends React.Component {
             <div>
                 {
                     tracks.map((track, key) => {
-                        return <div className="track" key={key}>{track.title}</div>;
+                        return (
+                            <div className="track" key={key}>
+                                {track.title}
+                                <button onClick={() => this.setState({ [key]: !this.state[key] })} type="button">
+                                    { this.state[key] ? 'Dislike' : 'Like' }
+                                </button>
+                            </div>
+                        );
                     })
                 }
             </div>
         );
     }
+
 
 }
 
