87412655098e66dec835756f40afe1059501cb31
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-19 14:52:06 -0400

    Added new tracks in state

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const tracks = [
+    {
+        title: 'Some track'
+    },
+    {
+        title: 'Some other track'
+    }
+];
+
+ReactDOM.render(
+    <div>
+        {
+            tracks.map((track) => {
+                return <div className="track">{track.title}</div>;
+            })
+        }
+    </div>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
