17942dc5784f7ebb9f21260395efec0fe49c31f7
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-20 08:48:25 -0400

    Refactoring code done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './stores/configureStore';
 import * as actions from './actions';
-import Stream from './components/Stream';
+import Stream from './components/Stream/index';
 import { Provider } from 'react-redux';
 
 const tracks = [
